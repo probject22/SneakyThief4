e7986cbfa3fec6a440fc43a9d4a37c37693b602d
Robert Stevens <robert@stevens.tk>
2015-04-08 18:58:40 +0200

    small update

--- src/core/Simulator.java
+++ src/core/Simulator.java
@@ -4,9 +4,15 @@ package core;
  * Created by Stan on 08/04/15.
  */
 public class Simulator {
-    private java.util.TreeMap<Double, core.sprite.Agent> Agents;
+	private boolean debug = true;
     
+	public Simulator(){
+		if (debug) System.err.println("The simulator has been started");
+	}
+	
     public static void main(String [] args){
     	new Simulator();
     }
+    
+	private java.util.TreeMap<Double, core.sprite.Agent> Agents;
 }
