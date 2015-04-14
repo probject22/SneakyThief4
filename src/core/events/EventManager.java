fafd8e5c0d92602200595eaf7b077067a4392c3d
Robert Stevens <robert@stevens.tk>
2015-04-14 14:12:52 +0200

    work on the event manager

--- src/core/events/EventManager.java
+++ src/core/events/EventManager.java
@@ -5,6 +5,9 @@ package core.events;
 
 import core.Map;
 import core.actions.ActionElement;
+import core.actions.Move;
+import core.actions.Turn;
+import core.actions.Wait;
 import core.sprite.Agent;
 import core.sprite.SpriteManager;
 
@@ -37,12 +40,52 @@ public class EventManager {
 	 * @param agent
 	 */
 	public void triggerEvent(ActionElement actionElement, Agent agent){
-		//TODO all the event handling
+		//TODO Get the timestamps of the actions
+		double timeStamp = 1;
+		if ( actionElement instanceof Turn){
+			//triger a vision event
+		}
 
-		//Generate events based on actionElement
-		//sound
-		//vision
+		/* this handles the move actionElement (THIS NEEDS TO BE IMPLEMENTED) */
+		else if ( actionElement instanceof Move){
+			//TODO dont trigger this event when a guard is entering a tower leaving a tower or when the agent turns for more then 45 degrees (page 4) 
+			generateVisionEvent(agent, timeStamp);
+			generateMoveSoundEvent(agent, timeStamp, ((Move)actionElement).speed);
+		}
 
-		//Give the events to the appropriate agents
+		/* handle the wait ActionElement */
+		else if ( actionElement instanceof Wait){
+			//trigger a vision event
+		}
+		
+	}
+	
+	private void generateVisionEvent(Agent agent, double timeStamp){
+		Vision vision = new Vision(timeStamp);
+		//TODO calculate what the agent can see and make a ?list? of it
+		agent.giveEvent(vision);
+	}
+	
+	private void generateMoveSoundEvent(Agent agent, double timeStamp, double speed){
+		Sound sound = new Sound(timeStamp);
+		double distence = 0;
+		if (speed >0 && speed < 0.5)
+			distence = 1;
+		else if (speed < 1)
+			distence = 3;
+		else if (speed < 2)
+			distence = 5;
+		else
+			distence = 10;
+		
+		//TODO put stuff in the sound event
+		
+		for (Agent tempAgent: spriteManager.getAgentList())
+			if (distenceBetweenAgents(agent, tempAgent) <= distence)
+				tempAgent.giveEvent(sound);
+	}
+	
+	private double distenceBetweenAgents(Agent agent1, Agent agent2){
+		return 0;
 	}
 }
