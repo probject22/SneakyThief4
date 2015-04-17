4786e38a04b142d8ed843ba2a88d8d0271a35308
RobClinch <r.clinch@hotmail.nl>
2015-04-17 14:32:29 +0200

    added components

--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -1,7 +1,19 @@
 package gui;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 
 public class ControlPanel extends JPanel {
-
+	public ControlPanel()
+	{
+		JButton startbutton = new JButton("Start");
+		JButton pausebutton = new JButton("Pause");
+		JButton stopbutton = new JButton("Stop");
+		JSpinner speedinput = new JSpinner();
+		this.add(startbutton);
+		this.add(pausebutton);
+		this.add(stopbutton);
+		this.add(speedinput);
+	}
 }
