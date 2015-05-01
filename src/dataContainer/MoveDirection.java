96dd36010db3fd74cb43c116b5fb7d7513c5ce93
Robert Stevens <robert@stevens.tk>
2015-05-01 23:48:55 +0200

    fixt a bug in the move direction file

--- src/dataContainer/MoveDirection.java
+++ src/dataContainer/MoveDirection.java
@@ -13,22 +13,22 @@ package dataContainer;
  */
 public enum MoveDirection {
 	/*	dx	|	dy	| start angle	| stop Angle	*/
-	N	( 1, 	0, 		67.5, 			112.5 		),
+	N	( 0, 	1, 		67.5, 			112.5 		),
 	NE	( 1, 	1, 		22.5, 			67.5 		),
 	
 	/*
 	 * East is split up in 2 parts one part that is bigger than 0 and one with is smaller then 360
 	 * there is also a normal E for move commands
 	 */
-	EP	( 0, 	1, 		0.0, 				22.5 		),
-	EN	( 0, 	1, 		337.5, 			360.0 		),
-	E	( 0, 	1, 		337.5, 			22.5 		),
+	EP	( 1, 	0, 		0.0, 				22.5 		),
+	EN	( 1, 	0, 		337.5, 			360.0 		),
+	E	( 1, 	0, 		337.5, 			22.5 		),
 	
-	SE	( -1, 	1, 		292.5, 			337.5 		),
-	S	( -1, 	0, 		247.5, 			292.5 		),
+	SE	( 1, 	-1, 		292.5, 			337.5 		),
+	S	( 0, 	-1, 		247.5, 			292.5 		),
 	SW	( -1, 	-1, 	202.5, 			247.5 		),
-	W	( 0, 	-1, 	157.5, 			202.5 		),
-	NW	( 1, 	-1, 	112.5, 			157.5 		);
+	W	( -1, 	0, 	157.5, 			202.5 		),
+	NW	( -1, 	1, 	112.5, 			157.5 		);
 	
 	MoveDirection(int x, int y, double startAngle, double endAngle){
 		this.x = x;
