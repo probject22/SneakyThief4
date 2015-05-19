b2106994afbcf66d60e37bf59af6d74ca471f8da
sina72 <s.maghaminick@student.maastrichtuniversity.nl>
2015-05-19 19:22:54 +0200

    small change inorder to run A* in a propper way

--- src/core/Algorithms/AStar/MapAStar.java
+++ src/core/Algorithms/AStar/MapAStar.java
@@ -28,7 +28,7 @@ public class MapAStar extends AStar<Coordinate, Coordinate> implements PathFinde
             node = node.parent;
         }
 
-        return moves.get(moves.size() -1);
+        return moves.get(0);
     }
     /**
      * Counts the amount of tiles between two coordinates on the map (assuming no obstructions).
