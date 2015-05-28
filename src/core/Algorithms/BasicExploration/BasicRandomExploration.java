9674cf32bce60944cf3fd4ea6ab2872cf05c2a82
Robert Stevens <robert@stevens.tk>
2015-05-28 10:38:30 +0200

    created a new map

---
+++ src/core/Algorithms/BasicExploration/BasicRandomExploration.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package core.Algorithms.BasicExploration;
+
+import core.Map;
+import core.sprite.Sprite;
+
+/**
+ * @author ing. R.J.H.M. Stevens
+ *
+ */
+public class BasicRandomExploration extends BasicExploration {
+
+	/**
+	 * @param sprite
+	 */
+	public BasicRandomExploration(Sprite sprite) {
+		super(sprite);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param sprite
+	 * @param beliefMap
+	 */
+	public BasicRandomExploration(Sprite sprite, Map beliefMap) {
+		super(sprite, beliefMap);
+		// TODO Auto-generated constructor stub
+	}
+
+}
