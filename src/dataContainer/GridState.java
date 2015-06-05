abb404f676f33216737e23adcd9b304ab2eebdc9
Robert Stevens <robert@stevens.tk>
2015-06-05 02:16:35 +0200

    bugfix

--- src/dataContainer/GridState.java
+++ src/dataContainer/GridState.java
@@ -14,10 +14,10 @@ public enum GridState {
 	Wall			(	Color.BLACK, 			false,			'-',		1.0,			1.0,			1.0			),
 	Sentry			(	Color.BLUE,				true,			'P',		1.0,			1.0,			1.0			),
 	Shade 			(	Color.GRAY,				true,			'S',		1.0,			1.0,			1.0			),
-	Window			(	Color.YELLOW,			true,			'W',		1.5,			3.0,			3.0			),
-	Door			(	new Color(102,51,0),	true,			'D',		2.5,			5.0,			5.0			),
+	Window			(	Color.YELLOW,			true,			'W',		5.0,			3.0,			5.0			),
+	Door			(	new Color(102,51,0),	true,			'D',		4.0,			5.0,			5.0			),
 	Target			(	Color.ORANGE,			true,			'T',		1.0,			1.0,			1.0			),
-	Tree			(	new Color(128,128,0),	false,			'B',		1.0,			1.0,			1.0			),
+	Tree			(	new Color(128,128,0),	false,			'B',		2.0,			2.0,			2.0			),
 	OuterWall		(	Color.DARK_GRAY,		false,			'+',		1.0,			1.0,			1.0			),
 	unknown			(	Color.RED,				true,			'9',		1.0,			1.0,			0.1			);
 
@@ -26,7 +26,7 @@ public enum GridState {
 	GridState(Color color, boolean moveable,  char fileVal, double agentCost, double thiefCost, double astarCost){
 		m_moveable = moveable;
 		this.color = color;
-		this.cost = 0;
+		this.cost = agentCost;
 		m_fileVal = fileVal;
 		this.thiefCost = thiefCost;
 		this.astarCost = astarCost;
